35275faab6f1d195e97b12d6349e8bb4ab0494a7
Kingminer7 <100500023+Kingminer7@users.noreply.github.com>
2023-04-14 19:26:36 -0400

    test

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const registry = require("./modules/Registry");
 
-app.get('/', homeHandler);
-
+registry.init(app);
+registry.registerRouter("test",__dirname);
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
